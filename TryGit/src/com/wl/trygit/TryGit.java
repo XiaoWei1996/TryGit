60d7383001d4bfe31aa798bbc2a1d1b10a702949
XiaoWei <36106635+XiaoWei1996@users.noreply.github.com>
2019-03-03 20:43:57 +0800

    Update TryGit.java

--- TryGit/src/com/wl/trygit/TryGit.java
+++ TryGit/src/com/wl/trygit/TryGit.java
@@ -3,5 +3,7 @@ package com.wl.trygit;
 public class TryGit {
 	public static void main(String[] args) {
 		System.out.println("hello hhha xixihei...");
+		System.out.print("修改后，再写回。。。。。。");
+
 	}
 }
